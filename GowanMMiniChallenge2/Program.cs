71c5b948e3625ecca1bcd142ddb376d8cc797196
Madeline <116022163+MJGowan@users.noreply.github.com>
2022-10-20 15:00:11 -0700

    Add files via upload

--- GowanMMiniChallenge2/Program.cs
+++ GowanMMiniChallenge2/Program.cs
@@ -4,6 +4,12 @@
 //In this challenge the user would enter two numbers, and the program must
 //recognize them as numbers with value, and add the values together.
 
+//peer review - Marcel R. It worked ass intended, I liked the use of if else
+//to choose paths with perimeters and how the while loop made use of the already
+//existing "Y" string to set the breakout condition. Data types for the
+//numbers could be redone to match Ie. all int or all long, but other than
+//that a great and fun program!
+
 Console.Clear();
 
 Console.Write("This is a math game, wanna play? Y or N: ");
